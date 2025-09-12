b87717a68ab946b729087c70860a930ad6fbc7ee
fennecpixels <agentolizx@gmail.com>
2025-09-12 18:52:36 +0100

    Create link_validate

---
+++ .github/workflows/link_validate.py
@@ -0,0 +1,59 @@
+import os
+import regex
+from urllib.parse import quote as url
+
+DIR = r'docs'
+
+class File():
+    def __init__(self,path,name) -> None:
+        self.path: str = path
+        self.name: str = name
+        self.url = url(os.path.join(path,name))
+
+    def __repr__(self) -> str:
+        return self.name
+    
+    def __str__(self) -> str:
+        return self.name
+
+    @staticmethod
+    def index_markdown_files(dir = DIR) -> list:
+        '''Indexes all markdown files based off an input directory.'''
+        output = []
+        for (dir_path, dir_names, file_names) in os.walk(DIR):
+           for file in file_names:
+               if file.endswith('.md'):
+                   output.append(File(path=os.path.join(dir_path,file),name=file))
+        return output
+
+def check_markdown_links(file: File):
+    '''Checks local markdown links. If inaccurate, removes them.'''
+    with open(file.path,'r+',encoding='utf-8') as f:
+        data = f.read()
+        links = regex.findall(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data)
+        if len(links) == 0:
+            return
+        else:
+            compare_to = list(map(lambda x: url(x.name),files))
+            for item in links:
+                compare = regex.sub(pattern=r"^.*/",string=url(item[1]),repl='',count=0,flags=regex.UNICODE)
+                if compare not in compare_to:
+                    data = regex.sub(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data,repl=r'\1',count=0,flags=regex.UNICODE)
+                    f.write(data)
+                    
+                
+                
+
+
+
+
+files = File.index_markdown_files()
+
+broken_links=[]
+
+for item in files:
+    try:
+        check_markdown_links(item)
+    except:
+        print(f"Error with {item}")
+
