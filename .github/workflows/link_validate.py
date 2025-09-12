2080bfe138da35741e994a62a74c6e5bd1c12466
fennecpixels <agentolizx@gmail.com>
2025-09-12 18:56:43 +0100

    Fix read/write structure

--- .github/workflows/link_validate.py
+++ .github/workflows/link_validate.py
@@ -8,7 +8,6 @@ class File():
     def __init__(self,path,name) -> None:
         self.path: str = path
         self.name: str = name
-        self.url = url(os.path.join(path,name))
 
     def __repr__(self) -> str:
         return self.name
@@ -28,28 +27,21 @@ class File():
 
 def check_markdown_links(file: File):
     '''Checks local markdown links. If inaccurate, removes them.'''
-    with open(file.path,'r+',encoding='utf-8') as f:
+    with open(file.path,'r',encoding='utf-8') as f:
         data = f.read()
-        links = regex.findall(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data)
-        if len(links) == 0:
-            return
-        else:
-            compare_to = list(map(lambda x: url(x.name),files))
-            for item in links:
-                compare = regex.sub(pattern=r"^.*/",string=url(item[1]),repl='',count=0,flags=regex.UNICODE)
-                if compare not in compare_to:
-                    data = regex.sub(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data,repl=r'\1',count=0,flags=regex.UNICODE)
-                    f.write(data)
-                    
-                
-                
-
-
-
-
+    links = regex.findall(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data)
+    if len(links) == 0:
+        return
+    else:
+        compare_to = list(map(lambda x: url(x.name),files))
+        for item in links:
+            compare = regex.sub(pattern=r"^.*/",string=url(item[1]),repl='',count=0,flags=regex.UNICODE)
+            if compare not in compare_to:
+                data = regex.sub(pattern=r'\[([^\]]+)\]\(([^)]+(.md))\)',string=data,repl=r'\1',count=0,flags=regex.UNICODE)
+        with open(file.path,'w',encoding='utf-8') as f:
+            f.write(data)
+    
 files = File.index_markdown_files()
-
-broken_links=[]
 
 for item in files:
     try:
